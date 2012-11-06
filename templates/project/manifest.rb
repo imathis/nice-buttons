7befaaae668394affcdb12db962b8a0558b355f9
Brandon Mathis <brandon@imathis.com>
2012-11-05 22:29:19 -0600

    1.0.3 updated manifest to add the screenshot

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,6 +1,6 @@
-# Make sure you list all the project template files here in the manifest.
 stylesheet 'demo.scss', :media => 'screen, projection'
 html 'demo.html'
+image 'screenshot.png'
 
 description "Create beautiful CSS3 buttons from a single color"
 help "This will install a demo (HTML and Scss) to show you how to use nice-buttons"
